3fc604e402d716b4bdf57d5e1b2954d1ffc172bf
Francois Ripault <francois.ripault@epita.fr>
2012-09-05 09:40:49 +0200

    Patch typos

--- .zsh/files.zsh
+++ .zsh/files.zsh
@@ -1,15 +1,16 @@
 # Zsh file extensions aliases
 
-for $vim in .c .h .cc .hh .cpp .txt; do
+for vim in .c .h .cc .hh .cpp .txt; do
   alias -s $vim=vim;
 done;
 
-for $img in jpg jpeg png gif tiff bmp; do
+for img in jpg jpeg png gif tiff bmp; do
   alias -s $img=eog;
 done;
 
-alias -s ml  ='ocaml -init'
-alias -s v   ='coqtop -l'
-# no working sbcl right now
+## does not seem to workd with options
+# alias -s ml  ='/usr/bin/ocaml -init'
+# alias -s v   ='coqtop -l'
+# not working sbcl right now
 # alias -s lisp='sbcl --load'
-alias -s tex ='pdflatex'
+# alias -s tex ='pdflatex'
