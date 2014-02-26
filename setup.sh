1b5f4b0de69df770cbff3aeab675f2625363faf6
Francois Ripault <francois.ripault@epita.fr>
2014-02-25 18:29:15 -0800

    Add vundle as a submodule; Remove useless plugins. Update TODO and setup.sh accordinly

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 FILES=".vim .vimrc .zsh .zshrc .gitconfig .gitignore_global"
 
+echo "Creating symbolic links to configuration files"
 for f in $FILES; do
   ln -s `pwd`/$f ~/$f;
 done
 
+echo "Setup up vim folder"
 mkdir ~/.vim/tmp
+git submodule init
+git submodule update
