260a7e74f0cb951db39dc66aa27632d88aea60ec
Francois Ripault <francois.ripault@epita.fr>
2014-02-25 16:37:29 -0800

    Add file for setopts

---
+++ .zsh/setopt.zsh
@@ -0,0 +1,12 @@
+setopt no_beep # don't beep on error
+
+# Perform implicit tees or cats when multiple redirections are attempted
+setopt multios
+# ^ not sure if useful
+
+# setopt cdablevarS
+# ^ again ... not sure if useful
+
+# Enable parameter expansion, command substitution, and arithmetic expansion in
+# the prompt
+setopt prompt_subst
