81beb6b3d56c067deaf2eb01fb513487a1bb72f5
Francois Ripault <francois.ripault@epita.fr>
2014-02-25 16:36:49 -0800

    cleanup aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,29 +1,24 @@
 # zsh alias file
 
 alias vim='vim -p'
-
-
 alias g++='g++ -Wall -Werror -Wextra -pedantic'
 
 alias      cp='cp -v'
 alias      mv='mv -v'
 alias      rm='rm -v'
-alias    grep='grep -nPi --color=auto'
-alias prename='prename -v'
+
+alias    grep='grep -ni --color=auto'
 
 alias   :q='exit'
-alias   :q!='kill -9 $$'
-alias  die='kill -9 $$'
-alias tree='tree -dA'
-alias  mem='su -c mem'
+
 alias   ..='cd ..'
 
-alias     cpuu='ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10'
-alias memusage='ps -e -orss=,args= | sort -b -k1,1n|pr -TW$COLUMNS'
-
-alias sbcl='rlwrap sbcl'
 alias sizeof='du -ch . | tail -n 1'
 
+# Program aliases
+# FIXME: They should be disabled if the bin is not found
+
+alias sbcl='rlwrap sbcl'
 alias latexmk='latexmk -pdf'
 alias ocaml='rlwrap ocaml'
 alias coqtop='rlwrap coqtop'
